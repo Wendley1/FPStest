bbb3948c25a75bbb880a268d95a3e3b58bb1d333
Wendley Santos <wendleygames134@gmail.com>
2023-04-03 17:25:57 -0300

    Added M4
    Added M4 and make some changes in the gun data

--- Assets/Scripts/DestroyObj.cs
+++ Assets/Scripts/DestroyObj.cs
@@ -2,9 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TopGun : MonoBehaviour
+public class DestroyObj : MonoBehaviour
 {
     [SerializeField] private float time;
+
+    public DestroyObj() 
+    {
+        time = 0f;
+    }
+
+    public DestroyObj(float time)
+    {
+        this.time = time;
+    }
 
     private void Start()
     {
